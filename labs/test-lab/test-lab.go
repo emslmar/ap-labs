b24574186f5ffeef0375edc9d830baefb1eea72e
emslmar <A01636165@itesm.mx>
2021-08-14 01:46:11 +0000

    Solve test-lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	if len(os.Args) > 1 {
+		fmt.Print("Hello ")
+		for i := 1; i < len(os.Args); i++ {
+			fmt.Print(os.Args[i])
+			fmt.Print(" ")
+		}
+		fmt.Print("Welcome to the Jungle \n")
+	} else {
+		fmt.Println("You are missing the parameter required : name")
+	}
+
 }
