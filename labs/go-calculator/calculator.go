16614807bc9ed6e03ce3e0f8a071d7c0cf931c47
emslmar <A01636165@itesm.mx>
2021-09-01 04:54:48 +0000

    go-calculator-fixed

--- labs/go-calculator/calculator.go
+++ labs/go-calculator/calculator.go
@@ -12,54 +12,62 @@ import (
 // multiplication (3)
 func calc(operator int, values []int) int {
 	result := values[0]
+	fmt.Printf("%d", values[0])
 	if operator == 1 {
 		for i := 1; i < len(values); i++ {
 			result += values[i]
+			fmt.Printf(" + %d", values[i])
 		}
 	} else if operator == 2 {
 		for i := 1; i < len(values); i++ {
 			result -= values[i]
+			fmt.Printf(" - %d", values[i])
 		}
 	} else if operator == 3 {
 		for i := 1; i < len(values); i++ {
 			result *= values[i]
+			fmt.Printf(" * %d", values[i])
 		}
 	}
-	fmt.Printf("%d \n", result)
+	fmt.Printf(" = %d\n", result)
 	return result
 }
 
 func main() {
 	operator := 0
 	flag := true
-	if os.Args[1] == "add" {
-		operator = 1
-	} else if os.Args[1] == "sub" {
-		operator = 2
-	} else if os.Args[1] == "mult" {
-		operator = 3
+
+	if len(os.Args) <= 2 {
+		fmt.Printf("faltan parametros\n")
 	} else {
-		fmt.Printf("No se reconocio el operador, recuerda  que solo aceptamos las palabras sum, sub y mult\n")
-		flag = false
-	}
-
-	nValues := len(os.Args) - 2
-	arr := make([]int, nValues)
-	pos := 0
-
-	for i := 2; i < len(os.Args); i++ {
-		num, err := strconv.Atoi(os.Args[i])
-		if err == nil {
-			arr[pos] = num
-			pos++
+		if os.Args[1] == "add" {
+			operator = 1
+		} else if os.Args[1] == "sub" {
+			operator = 2
+		} else if os.Args[1] == "mult" {
+			operator = 3
 		} else {
-			fmt.Print("Se ingreso un valor incorrecto donde corresponde un numero\n")
+			fmt.Printf("No se reconocio el operador, recuerda  que solo aceptamos las palabras sum, sub y mult\n")
 			flag = false
 		}
 
-	}
-	if flag {
-		calc(operator, arr)
-	}
+		nValues := len(os.Args) - 2
+		arr := make([]int, nValues)
+		pos := 0
 
+		for i := 2; i < len(os.Args); i++ {
+			num, err := strconv.Atoi(os.Args[i])
+			if err == nil {
+				arr[pos] = num
+				pos++
+			} else {
+				fmt.Print("Se ingreso un valor incorrecto donde corresponde un numero\n")
+				flag = false
+			}
+
+		}
+		if flag {
+			calc(operator, arr)
+		}
+	}
 }
