73babc3aa7bd78d9d038e71e21148675a8e60b7e
emslmar <A01636165@itesm.mx>
2021-09-07 22:55:14 +0000

    Solve first-partial

--- challenges/first-partial/c-month-day/month_day.c
+++ challenges/first-partial/c-month-day/month_day.c
@@ -1,8 +1,103 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static char daytab[2][13] = {
+        {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+        {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+/* month_name:  return name of n-th month */
+char *month_name(int n) {
+
+   static char *name[] = {
+       "Illegal month",
+       "Jan", "Feb", "Mar",
+       "Apr", "May", "Jun",
+       "Jul", "Aug", "Sep",
+       "Oct", "Nov", "Dec"
+   };
+
+   return (n < 1 || n > 12) ? name[0] : name[n];
+}
 
 /* month_day function's prototype*/
-void month_day(int year, int yearday, int *pmonth, int *pday);
+void month_day(int year, int yearday, int *pmonth, int *pday){
+    int numMonth;
+    int copyYearDay = yearday;
+    int leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for(int i = 0; i <= 12; i++){
+        if(yearday <= daytab[leap][i]){
+            *pday = yearday;
+            *pmonth = i;
+            break;
+        }else{
+            yearday = yearday - daytab[leap][i];
+        }
+    }
 
-int main() {
+}
+
+int main(int argc, char** argv) {
+    //printf("%d\n", argc);
+    if(argc < 3){
+        printf("No se ingresaron los parametros suficientes\n");
+        return -1;
+    }else if(argc > 3){
+        printf("Se ingresaron demasiados parametros\n");
+        return -1;
+    }
+    
+
+    int year;
+    int yearday;
+    int leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    long lnum;
+    char *end;  
+    lnum = strtol(argv[1], &end, 10);
+    if(*end != '\0'){
+        printf("ERROR: el parametro del año no es numerico\n");
+        return 0;
+    }else{
+        year = atoi(argv[1]);
+    }
+
+    lnum = strtol(argv[2], &end, 10);
+    if(*end != '\0'){
+        printf("ERROR: el parametro del dia no es numerico\n");
+        return 0;
+    }else{
+        yearday = atoi(argv[2]);
+    }
+
+    if(year < 1){
+        printf("ERROR: el año es invalido\n");
+        return 0;
+    }else if(yearday > 365 && leap == 0){
+        printf("ERROR: dia excede los 365 dias del año en un año no bisiesto\n");
+        return 0;
+    }else if(yearday > 366 && leap == 1){
+        printf("ERROR: dia excede los 366 dias del año en un año bisiesto\n");
+        return 0;
+    }
+
+    
+    int month;
+    int day;
+    
+    month_day(year, yearday, &month, &day);
+
+    char *monthName = month_name(month);
+    printf("%s %02d, %d\n", monthName, day, year);
+
     return 0;
 }
+
+
+
+
+
+
+
+
--- challenges/first-partial/go-functions-methods/geometry.go
+++ challenges/first-partial/go-functions-methods/geometry.go
@@ -7,18 +7,30 @@
 //!+point
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+)
 
-type Point struct{ X, Y float64 }
+type Point struct{ x, y float64 }
+
+func (p Point) X() float64 { return p.x }
+
+func (p Point) Y() float64 { return p.y }
 
 // traditional function
 func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.X()-p.X(), (q.Y() - p.Y()))
 }
 
 // same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	res := math.Hypot(q.X()-p.X(), q.Y()-p.Y())
+	fmt.Printf("%.2f ", res)
+	return res
 }
 
 //!-point
@@ -31,15 +43,151 @@ type Path []Point
 // Distance returns the distance traveled along the path.
 func (path Path) Distance() float64 {
 	sum := 0.0
-	for i := range path {
-		if i > 0 {
-			sum += path[i-1].Distance(path[i])
-		}
+	for i := 1; i < len(path); i++ {
+		sum += path[i-1].Distance(path[i])
+		fmt.Printf("+ ")
+
 	}
+	sum += path[len(path)-1].Distance(path[0])
+	fmt.Printf("= ")
 	return sum
 }
 
+// Given three colinear points p, q, r, the function checks if
+//point q lies on line segment 'pr'
+func onSegment(p Point, q Point, r Point) bool {
+	if q.x <= math.Max(p.X(), r.X()) && q.X() >= math.Min(p.X(), r.X()) && q.Y() <= math.Max(p.Y(), r.Y()) && q.Y() >= math.Min(p.Y(), r.Y()) {
+		return true
+	}
+	return false
+}
+
+// The main function that returns true if line segment 'p1q1'
+// and 'p2q2' intersect.
+func doIntersect(p1 Point, q1 Point, p2 Point, q2 Point) bool {
+
+	var o1 int = orientation(p1, q1, p2)
+	var o2 int = orientation(p1, q1, q2)
+	var o3 int = orientation(p2, q2, p1)
+	var o4 int = orientation(p2, q2, q1)
+
+	// General case
+	if o1 != o2 && o3 != o4 {
+		return true
+	}
+
+	// Special Cases
+	// p1, q1 and p2 are colinear and p2 lies on segment p1q1
+	if o1 == 0 && onSegment(p1, p2, q1) {
+		return true
+	}
+
+	// p1, q1 and q2 are colinear and q2 lies on segment p1q1
+	if o2 == 0 && onSegment(p1, q2, q1) {
+		return true
+	}
+
+	// p2, q2 and p1 are colinear and p1 lies on segment p2q2
+	if o3 == 0 && onSegment(p2, p1, q2) {
+		return true
+	}
+
+	// p2, q2 and q1 are colinear and q1 lies on segment p2q2
+	if o4 == 0 && onSegment(p2, q1, q2) {
+		return true
+	}
+
+	return false // Doesn't fall in any of the above cases
+}
+
+func orientation(p Point, q Point, r Point) int {
+	res := (q.Y()-p.Y())*(r.X()-q.X()) -
+		(q.X()-p.X())*(r.Y()-q.Y())
+
+	if res == 0 {
+		return 0
+	} // colinear
+
+	if res > 0 {
+		return 1
+	} //clockwise
+	return 2 //counterclockwise
+}
+
+func generatePoints(pointsArr []Point, n int) {
+	for i := 0; i < n; i++ {
+		pointsArr[i] = Point{-100 + rand.Float64()*(100+100), -100 + rand.Float64()*(100+100)}
+	}
+}
+
 func main() {
+	flag := true
+	var sides int
+
+	if len(os.Args) <= 1 {
+		fmt.Printf("No se recibio el numero de lados\n")
+		flag = false
+	} else if len(os.Args) > 2 {
+		fmt.Printf("Se recibieron demasiados parametros\n")
+		flag = false
+	} else {
+		num, err := strconv.Atoi(os.Args[1])
+		if err == nil {
+			if num >= 3 {
+				sides = num
+				fmt.Printf(" - Generating a [%d] sides figure\n", sides)
+			} else {
+				flag = false
+				fmt.Printf("Tiene que tener mas de dos lados para ser una figura geometrica \n")
+			}
+
+		} else {
+			fmt.Print("Se ingreso un valor incorrecto donde corresponde el numero de puntos\n")
+			flag = false
+		}
+	}
+
+	if flag {
+		//generate n sides or paths
+		var pointsArr []Point = make([]Point, sides)
+		generatePoints(pointsArr, sides)
+		pointsArr[0] = Point{-100 + rand.Float64()*(100+100), -100 + rand.Float64()*(100+100)}
+		pointsArr[1] = Point{-100 + rand.Float64()*(100+100), -100 + rand.Float64()*(100+100)}
+
+		for i := 2; i < sides; i++ {
+			pointsArr[i] = Point{-100 + rand.Float64()*(100+100), -100 + rand.Float64()*(100+100)}
+			//check if it intersects with any previous
+			for j := 0; j < i-2; j++ { //nunca va a chocar con el anterior
+				if doIntersect(pointsArr[j], pointsArr[j+1], pointsArr[i-1], pointsArr[i]) {
+					i = i - 1
+					break
+				}
+				if i == sides-1 {
+					if j == 0 {
+						continue
+					} else if doIntersect(pointsArr[j], pointsArr[j+1], pointsArr[i], pointsArr[0]) {
+						i = i - 1
+						break
+					}
+				}
+			}
+
+		}
+		//falta verificar el ultimo
+
+		fmt.Printf(" - Figure's vertices\n")
+		for i := 0; i < sides; i++ {
+			fmt.Printf("   - (%6.1f, %6.1f)\n", pointsArr[i].X(), pointsArr[i].Y())
+		}
+
+		fmt.Printf(" - Figure's Perimeter\n")
+		var camino Path = pointsArr
+		fmt.Printf("   - ")
+		fmt.Printf("%f \n", camino.Distance())
+
+		//pointsArr.Distance()
+		//check points dont intersect with each other
+	}
 }
 
 //!-path
