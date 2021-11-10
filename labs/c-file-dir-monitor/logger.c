e44ccb9a807c8771b71a70cac80b43f9f530d787
emslmar <A01636165@itesm.mx>
2021-11-10 20:35:32 +0000

    Solve c-file-dir-monitor

--- labs/c-file-dir-monitor/logger.c
+++ labs/c-file-dir-monitor/logger.c
@@ -1,23 +1,98 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+#include <string.h>
+#include <syslog.h>
 #include "logger.h"
 
+//Emiliano Sloth
+
+int logN = 0; 
+
 int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
+    printf("Log mode changed to %s\n", logType);
+    if (strcmp(logType,"stdout") == 0){
+        logN = 0;
+    } else {
+        if (strcmp(logType,"syslog") == 0){
+            logN = 1;
+        }
+        else{
+            printf("Error: logtype is invalid");
+            return -1;
+        }   
+    }
     return 0;
 }
 
-int infof(const char *format, ...) {
-    return 0;
+
+int infof ( const char * format, ... )
+{
+    va_list args;
+    va_start (args, format);
+    if(logN == 1){
+        openlog("logger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_EMERG,format, args);
+        closelog();
+
+    } else {
+        vprintf (format, args);
+        printf("\n");
+    }
+    va_end (args);
+    return 1;
 }
 
-int warnf(const char *format, ...) {
-    return 0;
+int warnf ( const char * format, ... )
+{
+    va_list args;
+    va_start (args, format);
+    if(logN == 1){
+        openlog("logger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_EMERG,format, args);
+        closelog();
+    } else {
+        printf("WARN: ");
+        vprintf (format, args);
+        printf("\n");
+    }
+    va_end (args);
+    printf("\x1b[0m");
+    return 1;
 }
 
-int errorf(const char *format, ...) {
-    return 0;
+int errorf ( const char * format, ... )
+{
+    va_list args;
+    va_start (args, format);
+    if(logN == 1){
+        openlog("logger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_EMERG,format, args);
+        closelog();
+
+    } else {
+        printf("ERROR: ");
+        vprintf (format, args);
+        printf("\n");
+    }
+    va_end (args);
+    return 1;
 }
-int panicf(const char *format, ...) {
-    return 0;
+
+int panicf ( const char * format, ... )
+{
+    va_list args;
+    va_start (args, format);
+    if(logN == 1){
+        openlog("logger", LOG_PID | LOG_CONS, LOG_SYSLOG);
+        vsyslog(LOG_EMERG,format, args);
+        closelog();
+    } else {
+        printf("PANIC: ");
+        vprintf (format, args);
+        printf("\n");
+    }
+    va_end (args);
+    return 1;
 }
 
--- labs/c-file-dir-monitor/monitor.c
+++ labs/c-file-dir-monitor/monitor.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
+#include <sys/inotify.h>
+#include <ftw.h>
+#include <unistd.h>
 #include "logger.h"
+struct inotify_event *iNotifyEvent;
 
-int main(){
-    // Place your magic here
+//Emiliano Sloth
+int main(int argc, char** argv){
+	int iNotifyAPI;
+	int scan;
+    int flag = 1;
+    char buffer[1024 * (sizeof(struct inotify_event) + 16)];
+	int dirWatched;
+	char *watchSize;
+
+	if(argc == 2) {
+		iNotifyAPI = inotify_init();
+		if(iNotifyAPI == -1)
+			errorf("Couldn't create the INotifyAPI");
+		dirWatched = inotify_add_watch(iNotifyAPI, argv[1], IN_ALL_EVENTS);
+		if(dirWatched == -1)
+			errorf("Problem with directory watch creation");
+        infof("Starting File/Directory Monitor on %s", argv[1]);
+		while(flag == 1) {
+			scan = read(iNotifyAPI, buffer, 1024*(sizeof(struct inotify_event)+16));
+			for (watchSize=buffer; watchSize<buffer+scan;){
+				iNotifyEvent = (struct inotify_event*) watchSize;
+                if (iNotifyEvent->mask & IN_CREATE){
+					if(iNotifyEvent->mask & IN_ISDIR){
+                    	printf("[Directory - ");
+                	}else{
+                    	printf("[File - ");
+                	}
+		            infof("Create] - %s", iNotifyEvent->name);
+				}
+	            if (iNotifyEvent->mask & IN_DELETE){
+					if(iNotifyEvent->mask & IN_ISDIR){
+                    	printf("[Directory - ");
+                	}else{
+                    	printf("[File - ");
+                	}
+		            infof("Removal] - %s", iNotifyEvent->name);
+				}
+	            if (iNotifyEvent->mask & IN_MOVE){
+					if(iNotifyEvent->mask & IN_ISDIR){
+                    	printf("[Directory - ");
+                	}else{
+                    	printf("[File - ");
+                	}
+		            infof("Rename] - %s", iNotifyEvent->name);
+				}
+				watchSize += sizeof(struct inotify_event)+iNotifyEvent->len;
+			}
+		}
+		inotify_rm_watch(iNotifyAPI, dirWatched);
+		close(iNotifyAPI);
+	}
+	else
+		warnf("not enough parameter or too many parameters");
     return 0;
 }
